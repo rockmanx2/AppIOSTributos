8c71cb324e56f5e5c9605f39423a49b7a4fe67e8
user907631 <user907631@ny166.local>
2017-12-21 18:29:02 -0500

    Initial Commit

---
+++ AppIOSTributos/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  AppIOSTributos
+//
+//  Created by user907631 on 12/21/17.
+//  Copyright © 2017 user907631. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
